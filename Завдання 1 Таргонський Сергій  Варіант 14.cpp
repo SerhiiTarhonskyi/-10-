611223ef36d30f338e1f509f61ea79bdf796b5dc
SerhiiTarhonskyi <118011647+SerhiiTarhonskyi@users.noreply.github.com>
2022-11-29 20:01:55 +0200

    Add files via upload

---
+++ Завдання 1 Таргонський Сергій  Варіант 14.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <math.h>
+#include <stdio.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    const double PI = 3.141592653589793;
+    float R = 1740., S, V;
+
+    S = 4. * PI * R * R;
+    V = 4 * PI * pow(R, 3) / 3.;
+    cout << " \nВаріант 14 " << "\n";
+    cout.precision(0);
+    cout << fixed << "Площа поверхні: " << S << " км^2"
+        << "\nОб'єм: " << V << " км^3" << "\n";
+
+    system("pause");
+    return 0;
+}
