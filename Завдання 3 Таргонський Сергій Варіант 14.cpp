67358f4ecacd4acb8b991c7e7c6e354a1fcb499c
SerhiiTarhonskyi <118011647+SerhiiTarhonskyi@users.noreply.github.com>
2022-11-29 20:48:53 +0200

    Add files via upload

---
+++ Завдання 3 Таргонський Сергій Варіант 14.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <math.h>
+#include <stdio.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, z1, z2;
+    cout << " \nВаріант 14 " << "\n";
+    cout << "Введыть a: ";
+    cin >> a;
+    z1 = (cos(a) + sin(a)) / (cos(a) - sin(a));
+    cout << "z1: " << z1 << "\n";
+    z2 = (tan(2 * a) + (1 / cos(2 * a)));
+    cout << "z2: " << z2 << "\n";
+
+
+
+
+}
---
+++ Завдання 4 Таргонський Сергій Варіант 14.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <math.h>
+#include <stdio.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double x, p, a, y, b;
+    x = 11;
+    cout << "Варіант 14" << "\n";
+    cout << "Введіть значення другого параметра: " << "\n";
+    cin >> p;
+    b = x + pow(p, 3);
+    cout << "b: " << b << "\n";
+    a = log(abs(x));
+    cout << "a: " << a << "\n";
+    y = sqrt(x) * sin(a) + sqrt(b) * cos(x);
+    cout << "y: " << y << "\n";
+
+
+
+}
